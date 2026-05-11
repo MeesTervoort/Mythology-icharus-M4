50dcc3637466cdd839f8d20f1b363d4d5f5afa4a
MeesTervoort <39862@ma-web.nl>
2026-05-11 14:38:05 +0200

    combat startin to get along

--- Assets/scripts/CharacterBattle.cs
+++ Assets/scripts/CharacterBattle.cs
@@ -2,19 +2,26 @@ using UnityEngine;
 
 public class CharacterBattle : MonoBehaviour
 {
-   
-    
-
-    void Start()
+    public void Setup(bool isPlayerTeam)
     {
-        
+        if (isPlayerTeam)
+        {
+            transform.position = new Vector3(-2, 1, 0);
+        }
+        else
+        {
+            transform.position = new Vector3(2, 1, 0);
+        }
     }
 
-    void Update()
+    public Vector3 GetPosition()
     {
-        
+        return transform.position;
     }
 
-    
-
+    public void Attack(CharacterBattle targetCharacterBattle)
+    {
+        Vector3 attackdir = (targetCharacterBattle.GetPosition() - GetPosition()).normalized;
+        Debug.Log("Attack");
+    }
 }
--- Assets/scripts/battleHandler.cs
+++ Assets/scripts/battleHandler.cs
@@ -2,14 +2,43 @@ using UnityEngine;
 
 public class battleHandler : MonoBehaviour
 {
+    [SerializeField] public Transform character;
+    [SerializeField] public Transform enemy;
+    [SerializeField] public Transform Rat;
+
     void Start()
     {
-
+        SpawnPlayer();
+        SpawnEnemy(true);
+        SpawnEnemy(false);
     }
 
     void Update()
     {
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
 
+        }
     }
 
+    private void SpawnEnemy(bool isRat)
+    {
+        Vector3 position;
+        if (isRat)
+        {
+            position = new Vector3(7.5f, 0.5f, 0);
+            Instantiate(Rat, position, Quaternion.identity);
+        }
+        else
+        {
+            position = new Vector3(4, 2, 0);
+            Instantiate(enemy, position, Quaternion.identity);
+        }
+    }
+    private void SpawnPlayer()
+    {
+        Vector3 position = new Vector3(-4, 2, 0);
+        Instantiate(character, position, Quaternion.identity);
+     }
+    
 }
