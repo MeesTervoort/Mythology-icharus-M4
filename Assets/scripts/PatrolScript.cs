a705cab0ff382731825d4807cc3c068d24cd946b
MeesTervoort <39862@ma-web.nl>
2026-06-02 12:10:19 +0200

    partol enmemny done :3

--- Assets/scripts/PatrolScript.cs
+++ Assets/scripts/PatrolScript.cs
@@ -6,36 +6,36 @@ public class PatrolScript : MonoBehaviour
     public int currentWaypointIndex = 0;
     private float speed = 2f;
 
-    private float waitTime = 2f;
+    private float waitTime = 1f; //in seconds
     private float waitCounter = 0f;
     private bool isWaiting = false;
-    void Start()
-    {
-        
-    }
-
+    
     void Update()
     {
+        if (isWaiting == true)
+        {
+            waitCounter += Time.deltaTime;
+            if (waitCounter < waitTime)
+                return;
+            isWaiting = false;
+        }
+
         Transform wp = waypoints[currentWaypointIndex];
         if (Vector3.Distance(transform.position, wp.position) < 0.1f)
         {
+            transform.position = wp.position;
+            waitCounter = 0f;
+            isWaiting = true;
+
             currentWaypointIndex = (currentWaypointIndex + 1) % waypoints.Length;
         }
         else
         {
             transform.position = Vector3.MoveTowards(transform.position, wp.position, speed * Time.deltaTime);
+            transform.LookAt(transform.position);
         }
 
 
 
-        if (isWaiting)
-        {
-            waitCounter += Time.deltaTime;
-            if (waitCounter >= waitTime)
-                return;
-            isWaiting = false;
-
-
-        }
     }
 }
