1274cf5ba9cd53507c1e40d78e14c6667bc8d885
MeesTervoort <39862@ma-web.nl>
2026-06-15 14:49:12 +0200

    cutscene begin

--- Assets/scripts/Mees/World/GuardTouch.cs
+++ Assets/scripts/Mees/World/GuardTouch.cs
@@ -25,7 +25,8 @@ public class GuardTouch : MonoBehaviour
 
     private void Die()
     {
-        UnityEngine.SceneManagement.SceneManager.LoadScene(UnityEngine.SceneManagement.SceneManager.GetActiveScene().name);
+        UnityEngine.SceneManagement.SceneManager.LoadScene("DeathScreen");
+        //UnityEngine.SceneManagement.SceneManager.LoadScene(UnityEngine.SceneManagement.SceneManager.GetActiveScene().name);
     }
 
 
