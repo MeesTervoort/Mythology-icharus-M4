deb422078e6c5a8358b7ead26740656f03352365
MeesTervoort <39862@ma-web.nl>
2026-06-16 11:44:47 +0200

    cutscene & script cleanup

--- Assets/scripts/Mees/World/CutsceneManager.cs
+++ Assets/scripts/Mees/World/CutsceneManager.cs
@@ -1,18 +1,29 @@
 using UnityEngine;
+using UnityEngine.UI;
 
 public class CutsceneManager : MonoBehaviour
 {
     [SerializeField] private GameObject Canvas;
+    [SerializeField] private Image Image;
 
     [SerializeField] private Sprite[] CutsceneImage;
-
-    void Start()
-    {
-        
-    }
+    private int step;
 
     void Update()
     {
-        
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            if (step >= CutsceneImage.Length)
+            {
+                UnityEngine.SceneManagement.SceneManager.LoadScene("main");
+                step = 0;
+            }
+            else
+            {
+                Canvas.SetActive(true);
+                Image.sprite = CutsceneImage[step];
+                step++;
+            }
+        }
     }
 }
