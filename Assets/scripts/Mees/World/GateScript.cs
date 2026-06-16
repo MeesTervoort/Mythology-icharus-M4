b195967915818662ddf2f441f3d65d69b53551a2
MeesTervoort <39862@ma-web.nl>
2026-06-16 13:12:53 +0200

    blijkbaar nu mijn scene

--- Assets/scripts/Mees/World/GateScript.cs
+++ Assets/scripts/Mees/World/GateScript.cs
@@ -2,13 +2,12 @@ using UnityEngine;
 
 public class GateScript : MonoBehaviour
 {
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    private Animator animator;
     void Start()
     {
-        
+        animator = GetComponent<Animator>();
     }
 
-    // Update is called once per frame
     void Update()
     {
         
