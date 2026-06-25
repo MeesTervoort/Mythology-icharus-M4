01b7d1801e194117ce80c526aea3754db860a1aa
Pascal1808 <pascalhorden@gmail.com>
2026-06-25 10:02:07 +0200

    werkt weer

--- Assets/scripts/Pascal/Player/Player.cs
+++ Assets/scripts/Pascal/Player/Player.cs
@@ -25,6 +25,7 @@ public class Player : MonoBehaviour
     
     public static int Coins = 0;
     public static int Health = 10;
+    public static int Feathers = 0;
 
     private void Awake()
     {
