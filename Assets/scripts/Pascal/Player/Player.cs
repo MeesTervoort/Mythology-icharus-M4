3477a1160088219f47ccb313e3c17cc36222dc71
MeesTervoort <39862@ma-web.nl>
2026-06-25 10:50:03 +0200

    Revert "Merge branch 'Mees' into Pascal"
    This reverts commit 0452a908b356e98d5f89eee723f1fc0e55af2a8f, reversing
    changes made to 526deb6d83321c7496be00f2c49d8a76856c842d.

--- Assets/scripts/Pascal/Player/Player.cs
+++ Assets/scripts/Pascal/Player/Player.cs
@@ -11,7 +11,6 @@ public class Player : MonoBehaviour
     [SerializeField] private float turnSpeed = 150f;
     [SerializeField] public static float jumpForce = 5f;
     [SerializeField] public static float Strength = 5f;
-    [SerializeField] public static int Feathers = 0;
     public UnityEngine.UI.Image healthImage;
 
 
@@ -26,6 +25,7 @@ public class Player : MonoBehaviour
     
     public static int Coins = 0;
     public static int Health = 10;
+    [SerializeField] public static int Feathers = 0;
 
     private void Awake()
     {
