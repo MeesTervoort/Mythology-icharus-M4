0be243ee7d0623b6b8f632ba01c20807269174b3
MeesTervoort <39862@ma-web.nl>
2026-06-25 12:16:04 +0200

    WORLD BACK!! (pls)

--- Assets/scripts/Pascal/Player/Player.cs
+++ Assets/scripts/Pascal/Player/Player.cs
@@ -26,6 +26,7 @@ public class Player : MonoBehaviour
     
     public static int Coins = 0;
     public static int Health = 10;
+    [SerializeField] public static int Feathers = 0;
 
     private void Awake()
     {
