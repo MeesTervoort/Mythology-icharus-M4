2c24c946363fc754780f4d6f365983c737ccf942
Pascal1808 <pascalhorden@gmail.com>
2026-06-25 10:47:00 +0200

    Revert "Merge branch 'Pascal' of https://github.com/MeesTervoort/Mythology-icharus-M4 into Pascal"
    This reverts commit 29ede90c315dea186f470f527d0c64a45db925dd, reversing
    changes made to c4555017a8b1a83baee0981069d8ae4745e201c5.

---
+++ Assets/scripts/Mees/World/GateScript.cs
@@ -0,0 +1,15 @@
+using UnityEngine;
+
+public class GateScript : MonoBehaviour
+{
+    private Animator animator;
+    void Start()
+    {
+        animator = GetComponent<Animator>();
+    }
+
+    void Update()
+    {
+        
+    }
+}
